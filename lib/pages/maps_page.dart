1c4fa35b30c6a79f8fe9a2bb2a32045c15f487c3
mehmetakifkucukkaya <mehmet.akif.20021@gmail.com>
2024-03-18 22:31:15 +0300

    Created MapPages

--- lib/pages/maps_page.dart
+++ lib/pages/maps_page.dart
@@ -1,18 +1,45 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapsPage extends StatefulWidget {
   const MapsPage({super.key});
 
   @override
-  State<MapsPage> createState() => _MapsPageState();
+  State<MapsPage> createState() => MapsPageState();
 }
 
-class _MapsPageState extends State<MapsPage> {
+class MapsPageState extends State<MapsPage> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Maps'),
+    return Scaffold(
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: const CameraPosition(
+          zoom: 15,
+          target: LatLng(41.01166917218091, 28.983355687202312),
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId("Hünkar Sofrası"),
+            position: LatLng(41.012739410754655, 28.978807582307617),
+            infoWindow: InfoWindow(title: "Hünkar Sofrası"),
+          ),
+          const Marker(
+            markerId: MarkerId("Safran Restoran"),
+            position: LatLng(41.01000743662969, 28.98321517903099),
+            infoWindow: InfoWindow(title: "Safran Restoran"),
+          ),
+          const Marker(
+            markerId: MarkerId("Deniz Köşesi"),
+            position: LatLng(41.01398657862989, 28.979476592511833),
+            infoWindow: InfoWindow(title: "Deniz Köşesi"),
+          ),
+        },
       ),
     );
   }
